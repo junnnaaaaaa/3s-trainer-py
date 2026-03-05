be72025b7076dafe775482304ac576ab1b05d8e1
junnnaaaaaa <paxtonbennison@outlook.com>
2026-03-05 21:12:16 +0800

    failed dialogue input

--- source/main.py
+++ source/main.py
@@ -3,6 +3,28 @@ import random
 import string
 from PySide6 import QtCore, QtWidgets, QtGui
 LETTERS = [chr(i) for i in range(65, 89)]
+class letterDialogue(QtWidgets.QDialog):
+    def __init__(self, controller):
+        super().__init__()
+        self.edit = QtWidgets.QLineEdit("Enter first letter:")
+        self.button = QtWidgets.QPushButton("Enter")
+        self.laying = QtWidgets.QVBoxLayout()
+        self.laying.addWidget(self.edit)
+        self.laying.addWidget(self.button)
+        self.setLayout(self.laying)
+        self.button.clicked.connect(self.enter(controller))
+        self.firstLetter = ''
+    def enter(self, controller):
+        self.firstLetter = self.edit.text()
+        controller.comms = commMenu(self, False, self.firstLetter)
+        controller.stack.addWidget(controller.comms())
+        self.close()
+        return
+class letterPair(QtWidgets.QPushButton):
+    def __init__(self, controller, letter1: str, letter2: str):
+        super().__init__(letter1+letter2)
+        self.comm = ''
+        # self.menu = QtWidgets.QTabWidget()
 class homeMenu(QtWidgets.QWidget):
     def __init__(self, controller):
         super().__init__()
@@ -14,8 +36,8 @@ class homeMenu(QtWidgets.QWidget):
         self.laying.addWidget(self.text)
         self.laying.addWidget(self.edgeButton)
         self.laying.addWidget(self.cornerButton)
-        self.edgeButton.clicked.connect(lambda: controller.setPage(1, True, "C"))
-        self.cornerButton.clicked.connect(lambda: controller.setPage(1, False, "B"))
+        self.edgeButton.clicked.connect(lambda: controller.setPage(1, True, True))
+        self.cornerButton.clicked.connect(lambda: controller.setPage(1, False, True))
 class commMenu(QtWidgets.QWidget):
     def __init__(self, controller, pieceType: bool, letter: str):
         super().__init__()
@@ -35,12 +57,14 @@ class mainWindow(QtWidgets.QMainWindow):
         self.stack.addWidget(self.home)
         self.stack.addWidget(self.comms)
         self.setCentralWidget(self.stack)
+        self.dialog = letterDialogue(self)
         self.stack.setCurrentIndex(0)
-    def setPage(self, index: int, pieceTypeIn = False, letterIn = ''):
-        self.stack.removeWidget(self.comms)
-        self.comms = commMenu(self, pieceTypeIn, letterIn)
-        self.stack.addWidget(self.comms)
-        self.stack.setCurrentIndex(index)
+    def setPage(self, index: int, pieceTypeIn = False, letterIn = False):
+        if letterIn:
+            self.stack.removeWidget(self.comms)
+            self.dialog.show()
+        else:
+            self.stack.setCurrentIndex(index)
 
         
 
